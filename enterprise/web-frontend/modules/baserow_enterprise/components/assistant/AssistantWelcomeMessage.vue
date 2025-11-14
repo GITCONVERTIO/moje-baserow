c8932d6ca9a7bb8f9187c40161a590debdad68dc
Bram <bram2w@users.noreply.github.com>
2025-11-14 13:06:51 +0100

    Add suggestions to the welcome message (#4252)

--- enterprise/web-frontend/modules/baserow_enterprise/components/assistant/AssistantWelcomeMessage.vue
+++ enterprise/web-frontend/modules/baserow_enterprise/components/assistant/AssistantWelcomeMessage.vue
@@ -26,8 +26,30 @@
       {{ $t('assistantWelcomeMessage.question') }}
     </h2>
     <p class="assistant__welcome-subtitle">
-      {{ $t('assistantWelcomeMessage.subtitle') }}
+      {{
+        suggestions.length === 0
+          ? $t('assistantWelcomeMessage.subtitleWithoutSuggestions')
+          : $t('assistantWelcomeMessage.subtitle')
+      }}
     </p>
+    <a
+      v-for="suggestion in suggestions"
+      :key="suggestion.id"
+      class="assistant__suggestion"
+      @click="$emit('prompt', suggestion.prompt)"
+    >
+      <div class="assistant__suggestion-icon-wrapper">
+        <div class="assistant__suggestion-icon">
+          <i :class="suggestion.icon"></i>
+        </div>
+      </div>
+      <div class="assistant__suggestion-text">
+        <div class="assistant__suggestion-title">{{ suggestion.title }}</div>
+        <div class="assistant__suggestion-description">
+          {{ suggestion.prompt }}
+        </div>
+      </div>
+    </a>
   </div>
 </template>
 
@@ -42,6 +64,10 @@ export default {
       type: String,
       default: 'there',
     },
+    uiContext: {
+      type: Object,
+      default: () => ({}),
+    },
   },
   computed: {
     video() {
@@ -49,6 +75,75 @@ export default {
     },
     image() {
       return image
+    },
+    suggestions() {
+      let type = this.uiContext.applicationType || null
+      if (this.uiContext.table) {
+        type = 'table'
+      }
+      const mapping = {
+        null: [
+          {
+            id: 'database',
+            icon: 'iconoir-view-grid',
+            title: this.$t('assistantWelcomeMessage.promptCreateDatabaseTitle'),
+            prompt: this.$t(
+              'assistantWelcomeMessage.promptCreateDatabasePrompt'
+            ),
+          },
+          {
+            id: 'automation',
+            icon: 'baserow-icon-automation',
+            title: this.$t(
+              'assistantWelcomeMessage.promptCreateAutomationTitle'
+            ),
+            prompt: this.$t(
+              'assistantWelcomeMessage.promptCreateAutomationPrompt'
+            ),
+          },
+          {
+            id: 'how',
+            icon: 'iconoir-send-mail',
+            title: this.$t('assistantWelcomeMessage.promptInviteUsersTitle'),
+            prompt: this.$t('assistantWelcomeMessage.promptInviteUsersPrompt'),
+          },
+        ],
+        database: [
+          {
+            id: 'table',
+            icon: 'iconoir-view-grid',
+            title: this.$t('assistantWelcomeMessage.promptCreateTableTitle'),
+            prompt: this.$t('assistantWelcomeMessage.promptCreateTablePrompt'),
+          },
+          {
+            id: 'which-tables',
+            icon: 'iconoir-view-grid',
+            title: this.$t('assistantWelcomeMessage.promptWhichTablesTitle'),
+            prompt: this.$t('assistantWelcomeMessage.promptWhichTablesPrompt'),
+          },
+        ],
+        table: [
+          {
+            id: 'form',
+            icon: 'iconoir-submit-document',
+            title: this.$t('assistantWelcomeMessage.promptCreateFormTitle'),
+            prompt: this.$t('assistantWelcomeMessage.promptCreateFormPrompt'),
+          },
+          {
+            id: 'filter',
+            icon: 'iconoir-filter',
+            title: this.$t('assistantWelcomeMessage.promptCreateFilterTitle'),
+            prompt: this.$t('assistantWelcomeMessage.promptCreateFilterPrompt'),
+          },
+          {
+            id: 'table',
+            icon: 'iconoir-view-grid',
+            title: this.$t('assistantWelcomeMessage.promptCreateTableTitle'),
+            prompt: this.$t('assistantWelcomeMessage.promptCreateTablePrompt'),
+          },
+        ],
+      }
+      return mapping[type] || []
     },
   },
 }
